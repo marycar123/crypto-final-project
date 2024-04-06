218221054945f8176ad341b95ef42a68a961c9e0
Adin Zimmerman <adinz@email.unc.edu>
2024-04-06 14:11:12 -0400

    upload and download text files

---
+++ upload_download.py
@@ -0,0 +1,21 @@
+from pydrive.auth import GoogleAuth 
+from pydrive.drive import GoogleDrive 
+gauth = GoogleAuth() 
+drive = GoogleDrive(gauth)
+
+
+def file_upload(upload_file_list: list[str]) -> None:
+    for upload_file in upload_file_list: 
+        gfile = drive.CreateFile(None)
+        gfile.SetContentFile(upload_file) 
+        gfile.Upload()
+
+def file_download(file_name: str) -> None:
+
+    file_list = drive.ListFile({'q': f"'root' in parents and trashed=false and title = '{file_name}'"}).GetList() 
+    for file in file_list: 
+        print('title: %s, id: %s' % (file['title'], file['id']))
+    print(file_list)
+    for i, file in enumerate(sorted(file_list, key = lambda x: x['title']), start=1): 
+        print('Downloading {} file from GDrive ({}/{})'.format(file['title'], i, len(file_list))) 
+        file.GetContentFile(file['title'])
