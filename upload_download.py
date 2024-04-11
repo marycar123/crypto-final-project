c74e0fd210d63fad04be6d10041b4ac7b0867728
Adin Zimmerman <adinz@email.unc.edu>
2024-04-11 13:58:19 -0400

    finished

--- upload_download.py
+++ upload_download.py
@@ -19,3 +19,4 @@ def file_download(file_name: str) -> None:
     for i, file in enumerate(sorted(file_list, key = lambda x: x['title']), start=1): 
         print('Downloading {} file from GDrive ({}/{})'.format(file['title'], i, len(file_list))) 
         file.GetContentFile(file['title'])
+
