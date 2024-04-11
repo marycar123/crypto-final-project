a500087fff24e817d75a940f9dadbc09caccc97a
Adin Zimmerman <adinz@email.unc.edu>
2024-04-11 14:00:27 -0400

    comments

--- upload_download.py
+++ upload_download.py
@@ -11,7 +11,7 @@ def file_upload(upload_file_list: list[str]) -> None:
         gfile.Upload()
 
 def file_download(file_name: str) -> None:
-
+    #going to need to access global key, encrypt file_name and use that to get the encrypted file
     file_list = drive.ListFile({'q': f"'root' in parents and trashed=false and title = '{file_name}'"}).GetList() 
     for file in file_list: 
         print('title: %s, id: %s' % (file['title'], file['id']))
@@ -19,4 +19,3 @@ def file_download(file_name: str) -> None:
     for i, file in enumerate(sorted(file_list, key = lambda x: x['title']), start=1): 
         print('Downloading {} file from GDrive ({}/{})'.format(file['title'], i, len(file_list))) 
         file.GetContentFile(file['title'])
-
