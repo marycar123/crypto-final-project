530effa8f0e878ec2ba519224b7b87df1f690384
Jessica Bring <jbring@ad.unc.edu>
2024-04-14 17:52:08 -0400

    key encryption stuff - wip

--- main.py
+++ main.py
@@ -1,19 +1,31 @@
 
-from etoe import decryption, encryption, keygen
+from etoe import decryption, encryption, keygen, enc_name, dec_name
 
 
-def run(myFileName: str, keyFileName: str) -> bool:
+def run(myFileName: str, keyFileName: str, nameKeyFileName: str) -> bool:
 
     file = open(myFileName, "r")
     myFile = file.read()
     myFile = myFile.encode('utf-8')
-    
 
     #Key generation, will store key at this file 
-    key=keygen(keyFileName)
+    keygen(keyFileName)
     #encrypting file and giving the key store in that file's keyFile
     ct=encryption(myFile, keyFileName)     
+    #TODO: encrypt file name 
+
+    keygen(nameKeyFileName)
+    myFileName = myFileName.encode('utf-8')
+    encrypted_name = enc_name(myFileName, nameKeyFileName)
+
+
+
+    encryptedFile = open(encrypted_name + ".txt", "w")
+    encryptedFile.write(ct)
+    encryptedFile.close()
+
     #decrypting        
+
     result = decryption(ciphertext = ct, keyFileName = keyFileName)
 
     return result == myFile
@@ -21,8 +33,13 @@ def run(myFileName: str, keyFileName: str) -> bool:
 
 
 if __name__ == '__main__':
-    myFileName = input("Enter a file Name: ")
-    keyFileName = input("Where do you want to store your key? ")
+    # myFileName = input("Enter a file Name: ")
+    # keyFileName = input("Where do you want to store your key? ")
+    # nameKeyFileName = input("Where do you want the name key to be stored? ")
 
-    print(run(myFileName=myFileName, keyFileName=keyFileName))
+    myFileName = "thisFile.txt"
+    keyFileName="key_thisFile.txt"
+    nameKeyFileName = "nameKey_thisFile.txt"
+
+    print(run(myFileName=myFileName, keyFileName=keyFileName, nameKeyFileName=nameKeyFileName))
 
