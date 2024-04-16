13a1ed2ad89d1e65c38db56ca3ca462941800eba
Jessica Bring <jbring@ad.unc.edu>
2024-04-16 15:53:08 -0400

    associated data encrypted, format vars

--- etoe.py
+++ etoe.py
@@ -8,19 +8,20 @@ from cryptography.hazmat.primitives.asymmetric import ec
 
 nonce_ctr = 2**28
 
-def encryption(plaintext: bytes, keyFileName: str) -> bytes:
-    #global nonce_ctr
-    nonce_ctr = 2**28
-    file = open(keyFileName, "r")
+def encryption(plaintext: bytes, key_file_name: str, associated_data: bytes) -> bytes:
+    global nonce_ctr
+
+    file = open(key_file_name, "r")
     key = file.read()
 
     key = key.encode('utf-8')
     cipher = AESGCM(key)
 
-    #do we need a nonce???? associated data????
-    header = (nonce_ctr, keyFileName)
-    ct = cipher.encrypt(nonce=bytes(str(nonce_ctr), 'ascii'),data=plaintext, associated_data=keyFileName.encode('utf-8'))
-    #nonce_ctr += 1
+    header = nonce_ctr
+    
+
+    ct = cipher.encrypt(nonce=bytes(str(nonce_ctr), 'ascii'),data=plaintext, associated_data=associated_data)
+    nonce_ctr += 1
     return header, ct
 
 def keygen(fileName: str) -> bytes:
@@ -29,9 +30,9 @@ def keygen(fileName: str) -> bytes:
    file.close()
 
 
-def decryption(ciphertext: bytes, keyFileName: bytes) -> bytes:
+def decryption(ciphertext: bytes, key_file_name: bytes) -> bytes:
     global nonce_ctr
-    file = open(keyFileName, "r")
+    file = open(key_file_name, "r")
     key = file.read()
     key = key.encode('utf-8')
 
